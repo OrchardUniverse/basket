235cb810017ad2d74f042987c757cf3c3aa8b75c
tobe <tobeg3oogle@gmail.com>
2024-07-26 00:25:51 +0800

    Support maas list

--- basket/main.py
+++ basket/main.py
@@ -1,17 +1,32 @@
 import argparse
+from tabulate import tabulate
+
 from model_client import ModelClient
 from maas_config import MaaSConfig, MaaSProvider
 
-
 client = ModelClient()
-config = MaaSConfig("maas_config.yaml")
+
+# TODO: Hanle fail to parse yaml file
+maas_config = MaaSConfig("maas_config.yaml")
 
 
 def maas_list():
     print("Listing MaaS...")
 
+    maas_providers = maas_config.providers
+
+    # Construct table to print
+    data = [["Service", "Available", "Base URL"]]
+    align = ("left", "center", "left")
+    for provider in maas_providers:
+        data.append([provider.name, "*", provider.url])
+    print(tabulate(data, headers="firstrow", tablefmt="pretty", colalign=align))
+
 def maas_use(name):
     print(f"Using MaaS: {name}")
+
+def maas_reset():
+    pass
 
 def model_list():
     print("Listing models...")
@@ -19,6 +34,9 @@ def model_list():
 def model_use(name):
     print(f"Using model: {name}")
 
+def list_config():
+    print("Listing config...")
+    
 def query(text):
     print(f"Querying with: {text}")
 
@@ -32,6 +50,8 @@ def main():
     maas_subparsers.add_parser("list", help="List model as a service")
     maas_use_parser = maas_subparsers.add_parser("use", help="Use a specific MaaS")
     maas_use_parser.add_argument("name", type=str, help="Name of the MaaS to use")
+    maas_reset_parser = maas_subparsers.add_parser("reset", help="Reset auths of a specific MaaS")
+    maas_reset_parser.add_argument("name", type=str, help="Name of the MaaS to reset")
 
     # Model command
     model_parser = subparsers.add_parser("model", help="Model related commands")
@@ -39,6 +59,9 @@ def main():
     model_subparsers.add_parser("list", help="List models")
     model_use_parser = model_subparsers.add_parser("use", help="Use a specific model")
     model_use_parser.add_argument("name", type=str, help="Name of the model to use")
+
+    # Config command
+    query_parser = subparsers.add_parser("config", help="List the current config")
 
     # Query command
     query_parser = subparsers.add_parser("query", help="Perform a query")
@@ -56,6 +79,8 @@ def main():
             model_list()
         elif args.model_command == "use":
             model_use(args.name)
+    elif args.command == "config":
+        list_config()
     elif args.command == "query":
         query(args.text)
     else:
