b358f7287ef1bd3eb162422c74f0803462d4fb8c
tobe <tobeg3oogle@gmail.com>
2024-07-25 19:22:34 +0800

    Add the basic python project files

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# basket
-The common toolkit for all MaaS with OpenAI API
+# Basket
+
+## Introduction
+
+Basket is a common toolkit for all MaaS with OpenAI API. It provides a unified interface for interacting with different MaaS services, allowing developers to easily switch between different services without changing their code.
+
+## Features
+
+- Unified interface for interacting with different MaaS services
+- Support for multiple MaaS services
+- Easy to use and integrate with existing projects
+- Extensible and customizable for different use cases
+
+## Getting Started
+
+To get started with Basket, follow these steps:
+
+```
+pip install orchard-basket
+```
+
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="orchard-basket",
+    version="0.1.0",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        # Add your package dependencies here
+    ],
+    author="Orchard Universe",
+    description="Basket is a common toolkit for all MaaS with OpenAI API.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/OrchardUniverse/busket",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
